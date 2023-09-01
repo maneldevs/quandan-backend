c8886894e48537b66d448e3b90c93559831f5fcb
maneldevs <maneldevs@gmail.com>
2023-09-01 11:37:52 +0000

    Add i18n files and utc configuration

--- src/main/java/es/maneldevs/quandanbackend/QuandanBackendApplication.java
+++ src/main/java/es/maneldevs/quandanbackend/QuandanBackendApplication.java
@@ -1,7 +1,11 @@
 package es.maneldevs.quandanbackend;
 
+import java.util.TimeZone;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
 
 @SpringBootApplication
 public class QuandanBackendApplication {
@@ -9,5 +13,10 @@ public class QuandanBackendApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(QuandanBackendApplication.class, args);
 	}
+
+	@PostConstruct
+    public void init(){
+      TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+    }
 
 }
