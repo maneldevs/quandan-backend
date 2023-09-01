db17eba59f308125a52bef5581662aa7ed46218b
maneldevs <maneldevs@gmail.com>
2023-09-01 07:29:12 +0000

    first commit

---
+++ src/main/java/es/maneldevs/quandanbackend/QuandanBackendApplication.java
@@ -0,0 +1,13 @@
+package es.maneldevs.quandanbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuandanBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuandanBackendApplication.class, args);
+	}
+
+}
